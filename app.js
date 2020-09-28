73aba83c9f7c601cab0f9e2dd9af306b93d76d4a
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-28 07:29:41 -0500

    added a public folder for css to access those static pages in the server for the client's browser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
+
+app.use(express.static('public'));
+// you will use this code above to access your static page (css & images) in your local server for the client's browser to use
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
