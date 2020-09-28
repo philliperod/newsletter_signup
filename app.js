3714feb8a755c37cd9df0883a98f0ec2436da446
Phillipe Rodriguez <philliperod@gmail.com>
2020-09-28 07:47:42 -0500

    added app.post and test came back successful

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/', (req, response) => {
+  console.log('POST request received.');
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000.');
 });
